eff4e39e0473d7717cd16a3d287ebd0c8acf44ff
jerryspann <jerryspann0@gmail.com>
2025-03-07 10:17:59 -0800

    wip: change redirect to index on create

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post("/fruits", async (req, res) => {
         req.body.isReadyToEat = false;
     }
     await Fruit.create(req.body);
-    res.redirect("/fruits/new");
+    res.redirect("/fruits");
 });
 
 // index route sends a page that list all fruits
